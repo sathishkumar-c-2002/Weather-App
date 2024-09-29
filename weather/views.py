b873d0e937c050b92b2aed26d4ebd582a310f2b8
csathishkumar <csathishkumar2002@gmail.com>
2024-09-29 14:29:01 +0530

    Kelvin converted into Fahrenheit

--- weather/views.py
+++ weather/views.py
@@ -8,13 +8,16 @@ def index(request):
     city = 'salem'
     r = requests.get(url.format(city)).json()
     # print(r.text)
+    temp_kelvin = r['main']['temp']
+    temp_fahrenheit = round(temp_kelvin - 273.15) * 9/5 + 32
+    
     city_weather = {
         'city' : city,
-        'temperature'  : r['main']['temp'],
+        'temperature'  : temp_fahrenheit,                     #r['main']['temp'],
         'description' : r['weather'][0]['description'],
         'icon' : r['weather'][0]['icon']
     }
     
     context = {'city_weather' : city_weather}
-    print(city_weather)
+    # print(city_weather)
     return render(request,'weather/weather.html',context)
