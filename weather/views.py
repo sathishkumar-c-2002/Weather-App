0b1991d2312c911d29ff4abfdb714bfa0edd243e
csathishkumar <csathishkumar2002@gmail.com>
2024-09-29 14:02:13 +0530

    requests module imported and it works

--- weather/views.py
+++ weather/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+import requests
+
 
 # Create your views here.
 def index(request):
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid=70b5bd0b714a64559c26ef018e7ac27f'
+    city = 'Las Vegas'
+    r = requests.get(url.format(city))
+    print(r.text)
     return render(request,'weather/weather.html')
