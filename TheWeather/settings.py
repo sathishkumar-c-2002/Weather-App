04994d3b448a905ba6b90893bfd7faf5dedaba6d
csathishkumar <csathishkumar2002@gmail.com>
2024-09-29 20:45:38 +0530

    Created gooter.html and modified the look of the data

--- TheWeather/settings.py
+++ TheWeather/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -77,10 +77,16 @@ WSGI_APPLICATION = 'TheWeather.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_weather_app',
+        'HOST': 'localhost',
+        'USER':'root',
+        'PASSWORD':'sathish',
+        'PORT':'3306'     
+        
     }
 }
+
 
 
 # Password validation
--- weather/views.py
+++ weather/views.py
@@ -1,23 +1,43 @@
 from django.shortcuts import render
 import requests
+from .models import City
+from .forms import CityForm
 
 
 # Create your views here.
 def index(request):
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid=70b5bd0b714a64559c26ef018e7ac27f'
     city = 'salem'
-    r = requests.get(url.format(city)).json()
-    # print(r.text)
-    temp_kelvin = r['main']['temp']
-    temp_fahrenheit = round(temp_kelvin - 273.15) * 9/5 + 32
+     
+     
+    if request.method == 'POST':
+        # print(request.POST)
+        form = CityForm(request.POST)
+        form.save()
     
-    city_weather = {
-        'city' : city,
-        'temperature'  : temp_fahrenheit,                     #r['main']['temp'],
-        'description' : r['weather'][0]['description'],
-        'icon' : r['weather'][0]['icon']
-    }
+    form = CityForm()
     
-    context = {'city_weather' : city_weather}
+    cities = City.objects.all()
+    
+    weather_data = []
+    
+    for city in cities:
+        
+        r = requests.get(url.format(city)).json()
+        # print(r.text)
+        
+        temp_kelvin = r['main']['temp']
+        temp_fahrenheit = round(temp_kelvin - 273.15) * 9/5 + 32
+        
+        city_weather = {
+            'city' : city.name,
+            'temperature'  : temp_fahrenheit,                     #r['main']['temp'],
+            'description' : r['weather'][0]['description'],
+            'icon' : r['weather'][0]['icon']
+        }
+        
+        weather_data.append(city_weather)
+    print(weather_data)
+    context = {'weather_data' : weather_data,'form': form}
     # print(city_weather)
     return render(request,'weather/weather.html',context)
