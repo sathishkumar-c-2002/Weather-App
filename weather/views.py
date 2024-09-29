c239c10d9bee7b0be91d2b32b1db20973beff609
csathishkumar <csathishkumar2002@gmail.com>
2024-09-29 14:17:33 +0530

    API works good which displays the city's  data properly in terminal

--- weather/views.py
+++ weather/views.py
@@ -5,7 +5,14 @@ import requests
 # Create your views here.
 def index(request):
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid=70b5bd0b714a64559c26ef018e7ac27f'
-    city = 'Las Vegas'
-    r = requests.get(url.format(city))
-    print(r.text)
+    city = 'salem'
+    r = requests.get(url.format(city)).json()
+    # print(r.text)
+    city_weather = {
+        'city' : city,
+        'temperature'  : r['main']['temp'],
+        'description' : r['weather'][0]['description'],
+        'icon' : r['weather'][0]['icon']
+    }
+    print(city_weather)
     return render(request,'weather/weather.html')
